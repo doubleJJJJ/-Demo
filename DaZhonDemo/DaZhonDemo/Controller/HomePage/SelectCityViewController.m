6bb66af2a0ca3d705dd1b7c1e81396472048e14a
doubleJJ <517469920@qq.com>
2015-11-10 18:04:37 +0800

    添加选择城市功能

--- DaZhonDemo/DaZhonDemo/Controller/HomePage/SelectCityViewController.m
+++ DaZhonDemo/DaZhonDemo/Controller/HomePage/SelectCityViewController.m
@@ -7,21 +7,82 @@
 //
 
 #import "SelectCityViewController.h"
-
+#import "pinyin.h"
 @interface SelectCityViewController ()
-
+@property (nonatomic,strong) NSMutableDictionary *cityDic;
+@property (nonatomic,strong) NSArray *allKeys;
 @end
 
 @implementation SelectCityViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.cityDic = [NSMutableDictionary dictionary];
+    
+    NSString *path = [[NSBundle mainBundle]pathForResource:@"citys" ofType:@"plist"];
+    NSArray *citys = [NSArray arrayWithContentsOfFile:path];
+    
+    for (NSString *cityName in citys) {
+        char firstLetter = pinyinFirstLetter([cityName characterAtIndex:0]);
+        NSString *key = [NSString stringWithFormat:@"%c",firstLetter];
+        NSMutableArray *newCitys = [self.cityDic objectForKey:key];
+        //根据key创建首字母数组(一个key对应一个数组)，如果存在，则直接添加城市名
+        if (newCitys == nil) {
+            newCitys = [NSMutableArray array];
+            [self.cityDic setObject:newCitys forKey:key];
+        }
+        [newCitys addObject:cityName];
+    }
+    //把plist文件保存到桌面
+    [self.cityDic writeToFile:@"/Users/doubleJJ/Desktop/citys.plist"  atomically:YES];
+    self.allKeys = [self.cityDic.allKeys sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+        return [obj1 compare:obj2];
+    }];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma tableView dataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return self.cityDic.allKeys.count;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    NSString *key = self.allKeys[section];
+    NSArray *citys = [self.cityDic objectForKey:key];
+    return citys.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    
+    NSString *key = self.allKeys[indexPath.section];
+    NSArray *citys = [self.cityDic objectForKey:key];
+    
+    NSString *cityName = citys[indexPath.row];
+    cell.textLabel.text = cityName;
+    return cell;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+    return self.allKeys[section];
+}
+//添加索引
+-(NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView{
+    return self.allKeys;
+}
+
+#pragma tableView delegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSString *key = self.allKeys[indexPath.section];
+    NSArray *citys = [self.cityDic objectForKey:key];
+    NSString *cityName = citys[indexPath.row];
+    
+    NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
+    [ud setObject:cityName forKey:@"cityName"];
+    [ud synchronize];
+    [self.navigationController popToRootViewControllerAnimated:YES];
+//  [[NSNotificationCenter defaultCenter]postNotificationName:@"cityChange" object:nil userInfo:@{@"cityName":cityName}];
+    
 }
 
 /*
